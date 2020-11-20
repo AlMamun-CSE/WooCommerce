671595aa9ce15c14c151a1acce2908cc2f432927
AlMamun-CSE <almamuncse2020@gmail.com>
2020-11-20 12:34:15 +0600

    Create Splash Screen

---
+++ app/src/main/java/com/alhumdulillah/woocommerce/SplashScreenActivity.java
@@ -0,0 +1,25 @@
+package com.alhumdulillah.woocommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static final String TAG = "SplashScreenActivity";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Log.d(TAG, "onCreate: started.");
+
+        SystemClock.sleep(5000);
+        Intent loginIntent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+}
